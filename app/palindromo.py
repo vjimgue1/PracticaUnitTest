ce8beca441cc989cf0e3c7a6efe0ed50717789ae
vjimgue <101817874+vjimgue@users.noreply.github.com>
2024-11-26 19:23:16 +0100

    added test cases

--- app/palindromo.py
+++ app/palindromo.py
@@ -4,7 +4,6 @@ Ignora espacios, mayúsculas y tildes.
 """
 def esPalindromo(cadena):
     # Convertir la cadena a minúsculas y eliminar caracteres no alfabéticos
-    cadena_limpia = ''.join(char.lower() for char in cadena if
-    char.isalnum())
+    cadena_limpia = ''.join(char.lower() for char in cadena if char.isalnum())
     # Comparar la cadena limpia con su reverso
     return cadena_limpia == cadena_limpia[::-1]
--- test/test_Palindromo.py
+++ test/test_Palindromo.py
@@ -1,24 +1,52 @@
 import unittest
 from app.charfun import esPalindromo
 
+
 class TestEsPalindromo(unittest.TestCase):
     def test_es_palindromo(self):
-        palindromos = ["abba", "a,b,a"]
+        palindromos = [
+            "abba",
+            "a,b,a",
+            "A man, a plan, a canal: Panama",
+            "Was it a car or a cat I saw?",
+            "121",
+            "No 'x' in Nixon",
+            "",
+            " ",
+            "a",
+        ]
         for frase in palindromos:
-            self.assertTrue(esPalindromo(frase))
-    
+            # De esta forma nos devuelve el valor del caso al fallar
+            with self.subTest(frase=frase):
+                self.assertTrue(esPalindromo(frase))
+
     def test_no_es_palindromo(self):
-        no_palindromos = ["abca", "a,c,d"]
+        no_palindromos = [
+            "abca", 
+            "a,c,d", 
+            "race a car", 
+            "tab a cat"
+        ]
         for frase in no_palindromos:
-            self.assertFalse(esPalindromo(frase))
-            
+            # De esta forma nos devuelve el valor del caso al fallar
+            with self.subTest(frase=frase):
+                self.assertFalse(esPalindromo(frase))
+
     def test_excepcion_tipo_incorrecto(self):
-        with self.assertRaises(TypeError):
-            esPalindromo(12)
-        with self.assertRaises(TypeError):
-            esPalindromo([1,2,4])
-        
-        
-            
+        lista_type_errors = [
+            12, 
+            [1, 2, 4], 
+            (1, 3, 5), 
+            1.3, 
+            {"a": 3, "1": 5, 4: 6}
+        ]
+        for error in lista_type_errors:
+            # De esta forma nos devuelve el valor del caso al fallar
+            with self.subTest(caso=error):
+                with self.assertRaises(TypeError):
+                    print(error)
+                    esPalindromo(error)
+
+
 if __name__ == "__main__":
-    unittest.main()            
+    unittest.main()
