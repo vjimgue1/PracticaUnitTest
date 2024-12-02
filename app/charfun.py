09128c353ed88eee130e97137dead90849ab95be
vjimgue <101817874+vjimgue@users.noreply.github.com>
2024-12-02 17:12:45 +0100

    implementación de solución test

--- app/charfun.py
+++ app/charfun.py
@@ -1,9 +1,11 @@
-
 """
 Función que verifica si una cadena es palíndroma.
 Ignora espacios, mayúsculas y tildes.
 """
 def esPalindromo(cadena):
+    # Comprobamos que la variable de entrada es una string
+    if not isinstance(cadena, str):
+        raise TypeError
     # Convertir la cadena a minúsculas y eliminar caracteres no alfabéticos
     cadena_limpia = ''.join(char.lower() for char in cadena if char.isalnum())
     # Comparar la cadena limpia con su reverso
